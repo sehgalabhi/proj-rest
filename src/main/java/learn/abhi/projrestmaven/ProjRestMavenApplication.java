5476b65ddc281ff573946632716df58accb662ee
asehgal <sehgal.abhi@gmail.com>
2022-02-23 13:59:04 +0100

    Initial setup

---
+++ src/main/java/learn/abhi/projrestmaven/ProjRestMavenApplication.java
@@ -0,0 +1,13 @@
+package learn.abhi.projrestmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjRestMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjRestMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/learn/abhi/projrestmaven/ProjRestMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.abhi.projrestmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjRestMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
